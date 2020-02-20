d4d57d345fd0788d841d0bf338c9fe2785063a78
Catalina <co319@ic.ac.uk>
2020-02-20 18:31:23 +0000

    Merge branch 'master' of C:\Users\Catalina Olteanu\Desktop\HashCode2020 with conflicts.

--- hashcode/src/Hashcode.java
+++ hashcode/src/Hashcode.java
@@ -1,2 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Hashcode {
+    public void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(""));
+        PrintStream printStream = new PrintStream(new File(""));
+
+        int numberOfBooks = scanner.nextInt();
+        int numberOfLibraries = scanner.nextInt();
+        int numberOfDays = scanner.nextInt();
+        int[] scores = new int[numberOfBooks];
+        Library[] libraries = new Library[numberOfLibraries];
+
+        for (int i = 0; i < numberOfBooks; ++i) {
+            scores[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < numberOfLibraries; ++i) {
+            libraries[i] = new Library(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+        }
+    }
 }
